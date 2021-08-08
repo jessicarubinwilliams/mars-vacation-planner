7aebec4c3dd66be2b94d4b522e9f888b42294430
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-08 16:48:16 -0700

    Add currently broken getWeatherElements fn
    Co-authored-by: Smita Raj <smita.raj12@gmail.com>

--- src/index.js
+++ src/index.js
@@ -23,3 +23,24 @@ $('#getInfo').click(function() {
       console.log(response);
     });
 });
+
+function getWeatherElements(response) {
+  if (response.sol_keys.length >= 1) {
+    const lastSol = parseInt(response.sol_keys[response.sol_keys.length-1]);
+    console.log(`lastSol: ${lastSol}`);
+    console.log(`typeof lastSol ${typeof lastSol}`);
+    const responseEntries = Object.entries(response);
+    responseEntries.forEach((response) => {
+      console.log(`response[0]: ${parseInt(response[0])}`);
+      console.log(`response[response.length-1]: ${parseInt(response[response.length-1])}`);
+      if (parseInt(response[0]) - lastSol === 0) {
+        console.log(response[1]);
+        $('#1-day').text(`${response[1]}`);
+      } else {
+        console.log(parseInt(response[0]) - lastSol);
+      };
+    });
+  } else {
+    $('#showErrors').text(`Something is amiss on Mars. NASA hasn't been able to gather any weather data for the past seven days`);
+  }
+}
