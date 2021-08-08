91eff6d43ae9d6ce5ee178309771a6249890df3c
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-08 10:48:57 -0700

    add fetch api for mars weather api
    Co-authored-by: Smita Raj <smita.raj12@gmail.com>

---
+++ src/weather-service.js
@@ -0,0 +1,14 @@
+export default class WeatherService {
+  static getWeather() {
+    return fetch(`https://api.nasa.gov/insight_weather/?api_key=${process.env.API_KEY}&feedtype=json&ver=1.0`)
+      .then(function(response) {
+        if(!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      });
+  }
+}
