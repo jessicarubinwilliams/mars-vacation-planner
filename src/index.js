9a27a7e1e74e59bcc326c339dd5a85d03143fae9
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-08 11:43:38 -0700

    Add call of Mars curiosity rover photos API
    Co-authored-by: Smita Raj <smita.raj12@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import WeatherService from './weather-service.js';
+import PhotoService from './photo-service.js';
 
 $('#getInfo').click(function() {
   WeatherService.getWeather()
     .then(function(response) {
+      console.log(response);
+    });
+  PhotoService.getPhoto()
+    .then(function(response){
       console.log(response);
     });
 });
