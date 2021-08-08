4a4608814f181ec0ab156ef1fa563aff8f4fb90b
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-08 10:48:20 -0700

    add click fn for initiating api call to uil
    Co-authored-by: Smita Raj <smita.raj12@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import WeatherService from './weather-service.js';
+
+$('#getInfo').click(function() {
+  WeatherService.getWeather()
+    .then(function(response) {
+      console.log(response);
+    });
+});
