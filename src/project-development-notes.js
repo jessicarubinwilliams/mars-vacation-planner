29255678dbb3c2f309a5f95727c010212c2ac772
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-11 16:50:24 -0700

    Expand sample InSight: Mars Weather Service API response

--- src/project-development-notes.js
+++ src/project-development-notes.js
@@ -1,20 +1,92 @@
-const response = {
+export const marsWeatherApiResponse = {
   sol_keys: [ "259", "260", "261", "262", "263", "264", "265" ],
   259: {
     AT: { 
-      av: -71.233,
+      av: -71.259,
       ct: 326642,
       mn: -101.024,
       mx: -27.149
     },
     HWS: {
-      av: 4.35,
+      av: 4.259,
       ct: 154146,
       mn: 0.156,
       mx: 17.617
     },
     PRE: {
-      av: 761.006,
+      av: 761.259,
+      ct: 163012,
+      mn: 742.1498,
+      mx: 780.3891
+    },
+    WD: {
+      most_common: null,
+      8: {
+        compass_degrees: 180.0,
+        compass_point: "S",
+        compass_right: 0.0,
+        compass_up: -1.0,
+        ct: 17699
+      },
+      9: {
+        compass_degrees: 202.5,
+        compass_point: "SSW", 
+        compass_right: -0.382683432365,
+        compass_up: -0.923879532511,
+        ct: 28551 }, 
+      10: {
+        compass_degrees: 225.0,
+        compass_point: "SW",
+        compass_right: -0.707106781187,
+        compass_up: -0.707106781187,
+        ct: 27124
+      }
+    },
+    First_UTC: "2019-08-19T08:03:59Z",
+    Last_UTC: "2019-08-20T08:43:34Z",
+    Season: "winter"
+  },
+  260: {
+    AT: {
+      av: -75.260,
+      ct: 300789,
+      mn: -101.715,
+      mx: -28.634
+    },
+    PRE: {
+      av: 762.260,
+      ct: 149206,
+      mn: 741.1254,
+      mx: 777.796
+    },
+    WD: { 
+      most_common: {
+        compass_degrees: 202.5,
+        compass_point: "SSW260",
+        compass_right: -0.382683432365,
+        compass_up: -0.923879532511,
+        ct: 28551
+      }
+    },
+    First_UTC: ":2019-08-20T08:43:34Z",
+    Last_UTC: "2019-08-21T09:23:09Z",
+    Season: "winter"
+  },
+  261: {
+    AT: { 
+      av: -71.261,
+      ct: 326642,
+      mn: -101.024,
+      mx: -27.149
+    },
+    HWS: {
+      av: 4.261,
+      ct: 154146,
+      mn: 0.156,
+      mx: 17.617
+    },
+    PRE: {
+      av: 761.261,
       ct: 163012,
       mn: 742.1498,
       mx: 780.3891
@@ -22,7 +94,7 @@ const response = {
     WD: {
       most_common: { 
         compass_degrees: 202.5,
-        compass_point: "SSW",
+        compass_point: "SSW261",
         compass_right: -0.382683432365,
         compass_up: -0.923879532511,
         ct: 28551
@@ -52,22 +124,213 @@ const response = {
     Last_UTC: "2019-08-20T08:43:34Z",
     Season: "winter"
   },
-  260: {
-    AT: {
-      av: -75.95,
-      ct: 300789,
-      mn: -101.715,
-      mx: -28.634
+  262: {
+    AT: { 
+      av: -71.262,
+      ct: 326642,
+      mn: -101.024,
+      mx: -27.149
+    },
+    HWS: {
+      av: 4.262,
+      ct: 154146,
+      mn: 0.156,
+      mx: 17.617
     },
     PRE: {
-      av: 762.462,
-      ct: 149206,
-      mn: 741.1254,
-      mx: 777.796
+      av: 761.262,
+      ct: 163012,
+      mn: 742.1498,
+      mx: 780.3891
     },
-    WD: { most_common: null },
-    First_UTC: ":2019-08-20T08:43:34Z",
-    Last_UTC: "2019-08-21T09:23:09Z",
+    WD: {
+      most_common: { 
+        compass_degrees: 202.5,
+        compass_point: "SSW262",
+        compass_right: -0.382683432365,
+        compass_up: -0.923879532511,
+        ct: 28551
+      },
+      8: {
+        compass_degrees: 180.0,
+        compass_point: "S",
+        compass_right: 0.0,
+        compass_up: -1.0,
+        ct: 17699
+      },
+      9: {
+        compass_degrees: 202.5,
+        compass_point: "SSW", 
+        compass_right: -0.382683432365,
+        compass_up: -0.923879532511,
+        ct: 28551 }, 
+      10: {
+        compass_degrees: 225.0,
+        compass_point: "SW",
+        compass_right: -0.707106781187,
+        compass_up: -0.707106781187,
+        ct: 27124
+      }
+    },
+    First_UTC: "2019-08-19T08:03:59Z",
+    Last_UTC: "2019-08-20T08:43:34Z",
     Season: "winter"
-  }
+  },
+  263: {
+    AT: { 
+      av: -71.263,
+      ct: 326642,
+      mn: -101.024,
+      mx: -27.149
+    },
+    HWS: {
+      av: 4.263,
+      ct: 154146,
+      mn: 0.156,
+      mx: 17.617
+    },
+    PRE: {
+      av: 761.263,
+      ct: 163012,
+      mn: 742.1498,
+      mx: 780.3891
+    },
+    WD: {
+      most_common: { 
+        compass_degrees: 202.5,
+        compass_point: "SSW263",
+        compass_right: -0.382683432365,
+        compass_up: -0.923879532511,
+        ct: 28551
+      },
+      8: {
+        compass_degrees: 180.0,
+        compass_point: "S",
+        compass_right: 0.0,
+        compass_up: -1.0,
+        ct: 17699
+      },
+      9: {
+        compass_degrees: 202.5,
+        compass_point: "SSW", 
+        compass_right: -0.382683432365,
+        compass_up: -0.923879532511,
+        ct: 28551 }, 
+      10: {
+        compass_degrees: 225.0,
+        compass_point: "SW",
+        compass_right: -0.707106781187,
+        compass_up: -0.707106781187,
+        ct: 27124
+      }
+    },
+    First_UTC: "2019-08-19T08:03:59Z",
+    Last_UTC: "2019-08-20T08:43:34Z",
+    Season: "winter"
+  },
+  264: {
+    AT: { 
+      av: -71.264,
+      ct: 326642,
+      mn: -101.024,
+      mx: -27.149
+    },
+    HWS: {
+      av: 4.264,
+      ct: 154146,
+      mn: 0.156,
+      mx: 17.617
+    },
+    PRE: {
+      av: 761.264,
+      ct: 163012,
+      mn: 742.1498,
+      mx: 780.3891
+    },
+    WD: {
+      most_common: { 
+        compass_degrees: 202.5,
+        compass_point: "SSW264",
+        compass_right: -0.382683432365,
+        compass_up: -0.923879532511,
+        ct: 28551
+      },
+      8: {
+        compass_degrees: 180.0,
+        compass_point: "S",
+        compass_right: 0.0,
+        compass_up: -1.0,
+        ct: 17699
+      },
+      9: {
+        compass_degrees: 202.5,
+        compass_point: "SSW", 
+        compass_right: -0.382683432365,
+        compass_up: -0.923879532511,
+        ct: 28551 }, 
+      10: {
+        compass_degrees: 225.0,
+        compass_point: "SW",
+        compass_right: -0.707106781187,
+        compass_up: -0.707106781187,
+        ct: 27124
+      }
+    },
+    First_UTC: "2019-08-19T08:03:59Z",
+    Last_UTC: "2019-08-20T08:43:34Z",
+    Season: "winter"
+  },
+  265: {
+    AT: { 
+      av: -71.265,
+      ct: 326642,
+      mn: -101.024,
+      mx: -27.149
+    },
+    HWS: {
+      av: 4.265,
+      ct: 154146,
+      mn: 0.156,
+      mx: 17.617
+    },
+    PRE: {
+      av: 761.265,
+      ct: 163012,
+      mn: 742.1498,
+      mx: 780.3891
+    },
+    WD: {
+      most_common: { 
+        compass_degrees: 202.5,
+        compass_point: "SSW265",
+        compass_right: -0.382683432365,
+        compass_up: -0.923879532511,
+        ct: 28551
+      },
+      8: {
+        compass_degrees: 180.0,
+        compass_point: "S",
+        compass_right: 0.0,
+        compass_up: -1.0,
+        ct: 17699
+      },
+      9: {
+        compass_degrees: 202.5,
+        compass_point: "SSW", 
+        compass_right: -0.382683432365,
+        compass_up: -0.923879532511,
+        ct: 28551 }, 
+      10: {
+        compass_degrees: 225.0,
+        compass_point: "SW",
+        compass_right: -0.707106781187,
+        compass_up: -0.707106781187,
+        ct: 27124
+      }
+    },
+    First_UTC: "2019-08-19T08:03:59Z",
+    Last_UTC: "2019-08-20T08:43:34Z",
+    Season: "winter"
+  },
+  validity_checks: {}
 };
