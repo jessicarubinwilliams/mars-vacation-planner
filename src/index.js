6e615a5c174fc733738b6bc63f5963037eb2f38e
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-11 17:35:48 -0700

    Fix portion of loop that dynamically displays API response info

--- src/index.js
+++ src/index.js
@@ -48,69 +48,65 @@ function getSpiritPhotoElements(response){
 function getWeatherElements(response) {
   if (response.sol_keys.length >= 1) {
     const lastSol = parseInt(response.sol_keys[response.sol_keys.length-1]);
-    console.log(lastSol);
     const responseEntries = Object.entries(response);
     responseEntries.forEach((entry) => {
       if (entry[0] === "sol_keys" || entry[0] === "validity_checks") {
         return;
       } else {
         const entrySol = parseInt(entry[0]);
-        console.log(entrySol);
         let windDirection = "";
         let avgTemp = "";
         let avgPressure = "";
         if (entry[1].WD.most_common === null || typeof entry[1].WD.most_common.compass_point !== "string" ) {
-          console.log("Something amiss with WD section of API response.");
+          // console.log("Something amiss with WD section of API response.");
           windDirection = "No valid wind direction data was collected for this day.";
-          console.log(windDirection);
         } else if (entry[1].WD.most_common !== null) {
-          console.log("Wind direction response okay.");
+          // console.log("Wind direction response okay.");
           windDirection = entry[1].WD.most_common.compass_point;
-          console.log(windDirection);
         } if (entry[1].AT === null || typeof entry[1].AT.av !== "number") {
-          console.log("Something amiss with AT section of API response.");
+          // console.log("Something amiss with AT section of API response.");
           avgTemp = "No valid temperature data was collected for this day.";
-          console.log(avgTemp);
         } else if (typeof entry[1].AT.av === "number") {
-          console.log("Temperature AT response okay.");
+          // console.log("Temperature AT response okay.");
           avgTemp = entry[1].AT.av;
-          console.log(avgTemp);
         } if (entry[1].PRE === null || typeof entry[1].PRE.av !== "number") {
-          console.log("Something amiss with PRE section of API response.");
+          // console.log("Something amiss with PRE section of API response.");
           avgPressure = "No valid atmospheric pressure data was collected for this day.";
-          console.log(avgPressure);
         } else if (typeof entry[1].PRE.av === "number") {
-          console.log("Pressure response okay.");
+          // console.log("Pressure response okay.");
           avgPressure = entry[1].PRE.av;
-          console.log(avgPressure);
         }
-        // let htmlToDisplay = `Average Temperature: ${avgTemp} degrees Fahrenheit, Average atmospheric pressure: ${avgPressure} Pa, Most common wind direction: ${windDirection}`;
-        // $("#weatherOutput").show();
-        // if (lastSol - entrySol === 0) {
-        //   console.log(`temp: ${entry[1].AT.av}`);
-        //   console.log(`pressure: ${entry[1].PRE.av}`);
-        //   console.log(`direction: ${entry[1].WD.most_common.compass_point}`);
-        //   $('#one-day').show();
-        //   $('#one-day').text(htmlToDisplay);
-        // } else if (lastSol - entrySol === 1) {
-        //   $('#two-day').show();
-        //   $('#two-days').text(htmlToDisplay);
-        // } else if (lastSol - entrySol === 2) {
-        //   $('#three-day').show();
-        //   $('#three-days').text(htmlToDisplay);
-        // } else if (lastSol - entrySol === 3) {
-        //   $('#four-day').show();
-        //   $('#four-days').text(htmlToDisplay);
-        // } else if (lastSol - entrySol === 4) {
-        //   $('#five-day').show();
-        //   $('#five-days').text(htmlToDisplay);
-        // } else if (lastSol - entrySol === 5) {
-        //   $('#six-day').show();
-        //   $('#six-days').text(htmlToDisplay);
-        // } else if (lastSol - entrySol === 6) {
-        //   $('#seven-day').show();
-        //   $('#seven-days').text(htmlToDisplay);
-        // }
+        let htmlToDisplay = `Average Temperature: ${avgTemp} degrees Fahrenheit, Average atmospheric pressure: ${avgPressure} Pa, Most common wind direction: ${windDirection}`;
+        $("#weatherOutput").show();
+        if (lastSol - entrySol === 0) {
+          console.log(entry[0]);
+          $('#one-day').show();
+          $('#one-day').append(htmlToDisplay);
+        } else if (lastSol - entrySol === 1) {
+          console.log(entry[0]);
+          $('#two-days').show();
+          $('#two-days').append(htmlToDisplay);
+        } else if (lastSol - entrySol === 2) {
+          console.log(entry[0]);
+          $('#three-days').show();
+          $('#three-days').append(htmlToDisplay);
+        } else if (lastSol - entrySol === 3) {
+          console.log(entry[0]);
+          $('#four-days').show();
+          $('#four-days').append(htmlToDisplay);
+        } else if (lastSol - entrySol === 4) {
+          console.log(entry[0]);
+          $('#five-days').show();
+          $('#five-days').append(htmlToDisplay);
+        } else if (lastSol - entrySol === 5) {
+          console.log(entry[0]);
+          $('#six-days').show();
+          $('#six-days').append(htmlToDisplay);
+        } else if (lastSol - entrySol === 6) {
+          console.log(entry[0]);
+          $('#seven-days').show();
+          $('#seven-days').append(htmlToDisplay);
+        }
       }
       console.log(`looped`);
     });
